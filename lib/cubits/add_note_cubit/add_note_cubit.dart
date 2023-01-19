29041537684ca0c08093f13e2f06e122d7a21318
Your Name <you@example.com>
2023-01-19 10:12:56 +0200

    add note method to add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -3,11 +3,27 @@
 
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/constants/constants.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_states.dart';
+import 'package:note_app/models/note_model.dart';
 
 class NotesCubit extends Cubit<AddNoteStates>{
   NotesCubit() : super(InitialState());
+  
 
+  addNote (NoteModel noteModel) async {
+    emit(AddNoteLoadingState());
+
+   try {
+  var notesBox = Hive.box<NoteModel>(AppConstants.notesBox);
+   await notesBox.add(noteModel);
+   emit(AddNoteSuccessState());
+} catch (e) {
+   emit(AddNoteFailureState(errMessage: e.toString()));
+}
+  
+  }  
   
   
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constants/constants.dart';
 import 'package:note_app/views/notes_view.dart';
+import 'models/note_model.dart';
 
-void main() {
+void main() async {
+
+
+  await Hive.initFlutter();
+  await Hive.openBox(AppConstants.notesBox);
+        Hive.registerAdapter(NoteModelAdapter());
+  
   runApp( NotesApp());
 }
 
