529339a2e1507c02325ea3f0fe547f37ab66a408
Your Name <you@example.com>
2023-01-19 11:15:24 +0200

    add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/constants/constants.dart';
+import 'package:note_app/simple_bloc_observer.dart';
 import 'package:note_app/views/notes_view.dart';
 import 'models/note_model.dart';
 
 void main() async {
-
-
   await Hive.initFlutter();
+  Bloc.observer = SimpleBlocObserver();
   await Hive.openBox(AppConstants.notesBox);
         Hive.registerAdapter(NoteModelAdapter());
   
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -3,20 +3,34 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:note_app/views/widgets/custom_button.dart';
-import 'package:note_app/views/widgets/custom_text_field.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_states.dart';
+import 'add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.only( bottom: 16 , left: 16 , right: 16  ),
-        child: AddNoteForm(),
-      ),
-    );
+    return BlocProvider(
+           create: (context) => AddNoteCubit() ,
+           child: BlocConsumer<AddNoteCubit,AddNoteStates>(
+             listener: (context, state) {
+                 if ( state is AddNoteFailureState ) {
+                   print('failure : ${state.errMessage}');
+                 }
+
+                 if ( state is AddNoteSuccessState ) {
+                   Navigator.pop(context);
+                 }
+             },
+             builder: (context,state) => const Padding(
+             padding: EdgeInsets.only( bottom: 16 , left: 16 , right: 16  ),
+             child: SingleChildScrollView(child: AddNoteForm()),
+           ),
+           ),
+     );
   }
 
   
@@ -26,55 +40,9 @@ class AddNoteBottomSheet extends StatelessWidget {
 }
 
 
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({super.key});
 
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
 
-class _AddNoteFormState extends State<AddNoteForm> {
-  
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled ;
 
-  String? title , subTitle ;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode ,
-      child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children:  [
-            const SizedBox(height: 32,),
-             CustomTextField(hint: 'title' , 
-                   maxLines: 1 ,
-                   onSaved: (value) {
-                      title = value ;
-                   } ,
-             ),
-            const SizedBox(height: 30,),
-             CustomTextField(hint: 'content' , maxLines: 5 , 
-               onSaved: (value) {
-                subTitle = value ; 
-               } ,
-            ),
-            const SizedBox(height: 25 ,) ,
-            CustomMaterialButton( buttonName: 'Add' , onPressed: (){
-              if (formKey.currentState!.validate()) {
-                  formKey.currentState!.save();
-              } else {
-                 autovalidateMode = AutovalidateMode.always ;
-                 setState(() {});
-              }
-            })
-          ],
-        ),
-    );
-  }
-}
 
 
 
